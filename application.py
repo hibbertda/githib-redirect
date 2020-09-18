0c0359c25abad5ffe7704c86cd4a186f82c2f437
Daniel Hibbert <hibbertda@outlook.com>
2020-09-18 00:46:56 -0400

    added test

--- application.py
+++ application.py
@@ -18,5 +18,9 @@ def redirectToURL():
         urlparts_list[1] = redirectURL
         return redirect(urlunparse(urlparts_list), code=301)
 
+@app.route('/test', methods=["GET", "POST"])
+def test():
+    return "test"
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)        
+    app.run(debug=False, host='0.0.0.0', port=80)        
